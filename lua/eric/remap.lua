7914712fc756e570221aab24197188dc6c46a583
Eric Bridgeford <ericbridgeford@erics-mbp.mynetworksettings.com>
2023-08-02 21:04:58 -0400

    Ooops

--- lua/eric/remap.lua
+++ lua/eric/remap.lua
@@ -1,12 +1,20 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Delete line below
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Jump up/down but keep cursor in center of screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- ???
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
